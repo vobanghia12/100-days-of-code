d2d6efe83e21933fee145b7c609f784246c56295
Nghia Vo <vobanghia12@gmail.com>
2021-12-09 23:56:54 -0500

    Bootstrap practice

---
+++ Bootstrap/parallax.js
@@ -0,0 +1,6 @@
+const image= document.getElementById('main-image');
+new simpleParallax(image,
+    {
+        scale: 1.6,
+        delay: 0.1,
+    }); // basically this code will active parallax effect for us 
